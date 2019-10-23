9c0f501a0ee87fe242cb7868ab3835a63b4a0764
Klob <ditchfieldcaleb@gmail.com>
2019-10-22 21:46:48 -0400

    Initial test routine in main, single pin setup

--- src/main.ino
+++ src/main.ino
@@ -1,10 +1,23 @@
 #include <FastLED.h>
+#include "variables.h"
+
+// Create the master led array
+// Declarations such as NUM_LEDS... are in variables.h
+CRGBArray<NUM_LEDS> leds;
 
 void setup() {
+  // Set up serial connection
+  Serial.begin(57600);
+  Serial.setTimeout(1500);
+  delay(1000);
 
+  // Setup LEDs
+  FastLED.addLeds<WS2812B, 5, GRB>(leds, NUM_LEDS);
 }
 
 void loop() {
-
-
+  static uint8_t hue=0;
+  leds(0, NUM_LEDS/2 - 1).fill_rainbow(hue++);
+  leds(NUM_LEDS/2, NUM_LEDS-1) = leds(NUM_LEDS/2-1,0);
+  FastLED.delay(30);
 }
