80483e2425a1a022465f4b4298fa830bfd67ddfa
Caleb Ditchfield <ditchfieldcaleb@gmail.com>
2019-11-22 16:39:54 -0500

    Create branch for hypercube, add in basic led defines.

--- include/vars.h
+++ include/vars.h
@@ -1,8 +1,15 @@
 // QUICK CHANGE variables
-#define NUM_LEDS 134
-#define LED_PIN 8
-#define CLOCK_PIN 9
 #define STARTUP_DEBUG false
 #define MAX_BRIGHT 128
 #define SERIAL_BAUDRATE 115200
 // QUICK CHANGE VARIABLES
+
+// HYPERCUBE LED PINS
+#define LED_PIN_ONE 2
+#define LED_PIN_TWO 3
+#define LED_PIN_THREE 4
+#define LED_PIN_FOUR 5
+#define LED_PIN_FIVE 6
+#define LED_PIN_SIX 7
+
+// HYPERCUBE LED COUNTS
